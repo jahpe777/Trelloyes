5584b953d597e6818a7fe9825ea7bf0b9bc5a7fa
James Higgs <james.y.higgs@gmail.com>
2019-08-18 16:20:50 -0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, {Component} from 'react';
-import './app.css';
-import List from './list.js';
+import './App.css';
+import List from './List.js';
 
 class App extends Component {
   static defaultProps = {
     store: {
       lists: [],
-      allCards: {}.
+      allCards: {},
     }
-  }
-}
+  };
 
     render () {
       const {store} = this.props;
-      const allLists = 
-      ));
+      return (
+        <main className="App">
+          <header className="App-header">
+            <h1>Trelloyes!</h1>
+          </header>
+          <div className="App-list">
+            {store.lists.map(list => (
+              <List
+                key={list.id}
+                header={list.header}
+                cards={list.cardIds.map(id => store.allCards[id])}
+                />
+            ))}
+          </div>
+        </main>
+      );
     }
-    return (
-      <main class="App">
-        <header class="App-header">
-          <h1>Trelloyes!</h1>
-        </header>
-        <div class="App-list">
-        </div>
-      </main>
-    );
   }
-}
 
 export default App;
--- src/list.js
+++ src/list.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import './list.css';
-import Card from './card.js';
+import './List.css';
+import Card from './Card.js';
 
 function List(props) {
-  const allCards = 
-
   return (
-    <section class="List">
-        <header class="List-header">
+    <section className="List">
+        <header className="List-header">
           <h2>{props.header}</h2>
         </header>
-        <div class="List-cards">
-          {allCards}
-        </div>
-        <button type="button" class="List-add-button">
+        <div className="List-cards">
+          {props.cards.map((card) =>
+            <Card
+              key={card.id}
+              title={card.title}
+              content={card.content}
+              />
+            )}
+        <button type="button" className="List-add-button">
             + Add Random Card
         </button>
+      </div>
     </section>
   );
 }
